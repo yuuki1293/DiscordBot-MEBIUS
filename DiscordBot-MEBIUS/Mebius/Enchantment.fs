96cd4f63975d7b7fbccd41b8b45b6f6add62e94f
yuuki1293 <yuuki1293@outlook.jp>
2021-12-24 22:37:19 +0900

    mebiusのエンチャントの判別共用型を実装

--- DiscordBot-MEBIUS/Mebius/Enchantment.fs
+++ DiscordBot-MEBIUS/Mebius/Enchantment.fs
@@ -21,4 +21,9 @@ type MebiusEnchantments =
         | Durability -> MebiusEnchantment(1, 10, "耐久力")
         | Mending -> MebiusEnchantment(1, 1, "修繕")
         | FireProtection -> MebiusEnchantment(6, 10, "火炎耐性")
+        | ProjectileProtection -> MebiusEnchantment(6, 10, "飛び道具耐性")
+        | ExplosionProtection -> MebiusEnchantment(6, 10, "爆発耐性")
+        | Respiration -> MebiusEnchantment(15, 3, "水中呼吸")
+        | WaterAffinity -> MebiusEnchantment(15, 1, "水中採掘")
+        | Unbreakable -> MebiusEnchantment(30, 1, "耐久無限")
         | _ -> failwith "todo"
