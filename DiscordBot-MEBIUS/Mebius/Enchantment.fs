c7e72aa6d1fbdf989e9aa425aec4d8038d546d96
yuuki1293 <yuuki1293@outlook.jp>
2021-12-24 23:25:53 +0900

    MebiusEnchantmentsにlevelを追加

--- DiscordBot-MEBIUS/Mebius/Enchantment.fs
+++ DiscordBot-MEBIUS/Mebius/Enchantment.fs
@@ -6,24 +6,23 @@ type MebiusEnchantment(unlockLevel: int, maxLevel: int, displayName: string) =
     let displayName = displayName
 
 type MebiusEnchantments =
-    | Protection
-    | Durability
-    | Mending
-    | FireProtection
-    | ProjectileProtection
-    | ExplosionProtection
-    | Respiration
-    | WaterAffinity
-    | Unbreakable
+    | Protection of level: int
+    | Durability of level: int
+    | Mending of level: int
+    | FireProtection of level: int
+    | ProjectileProtection of level: int
+    | ExplosionProtection of level: int
+    | Respiration of level: int
+    | WaterAffinity of level: int
+    | Unbreakable of level: int
     member this.Enchant =
         match this with
-        | Protection -> MebiusEnchantment(2, 10, "ダメージ軽減")
-        | Durability -> MebiusEnchantment(1, 10, "耐久力")
-        | Mending -> MebiusEnchantment(1, 1, "修繕")
-        | FireProtection -> MebiusEnchantment(6, 10, "火炎耐性")
-        | ProjectileProtection -> MebiusEnchantment(6, 10, "飛び道具耐性")
-        | ExplosionProtection -> MebiusEnchantment(6, 10, "爆発耐性")
-        | Respiration -> MebiusEnchantment(15, 3, "水中呼吸")
-        | WaterAffinity -> MebiusEnchantment(15, 1, "水中採掘")
-        | Unbreakable -> MebiusEnchantment(30, 1, "耐久無限")
-        | _ -> failwith "todo"
+        | Protection _ -> MebiusEnchantment(2, 10, "ダメージ軽減")
+        | Durability _ -> MebiusEnchantment(1, 10, "耐久力")
+        | Mending _ -> MebiusEnchantment(1, 1, "修繕")
+        | FireProtection _ -> MebiusEnchantment(6, 10, "火炎耐性")
+        | ProjectileProtection _ -> MebiusEnchantment(6, 10, "飛び道具耐性")
+        | ExplosionProtection _ -> MebiusEnchantment(6, 10, "爆発耐性")
+        | Respiration _ -> MebiusEnchantment(15, 3, "水中呼吸")
+        | WaterAffinity _ -> MebiusEnchantment(15, 1, "水中採掘")
+        | Unbreakable _ -> MebiusEnchantment(30, 1, "耐久無限")
